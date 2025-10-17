2a3773546e22a4a5bb8a4e2853f58ba915b48df9
moni27moni <mems250005027@iiti.ac.in>
2025-10-17 17:19:08 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+using namespace std;
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+int main() {
+    Color colors[5] = {
+        {255, 0, 0},      
+        {0, 255, 0},      
+        {0, 0, 255},      
+        {255, 255, 0},    
+      {255, 0, 255}    
+    };
+    for (int i = 0; i < 3; i++) {
+        colors[i].red = 255 - colors[i].red;
+        colors[i].green = 255 - colors[i].green;
+        colors[i].blue = 255 - colors[i].blue;
+    }
+    cout << "Updated RGB values after inverting the first 3 colors:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Color " << i + 1 << " -> R: " << colors[i].red 
+             << ", G: " << colors[i].green 
+             << ", B: " << colors[i].blue << endl;
+    }
+    return 0;
+}
