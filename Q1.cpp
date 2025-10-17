841eb1f31353df9297d97b436767f574013a9ab6
moni27moni <mems250005027@iiti.ac.in>
2025-10-17 16:11:18 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+struct item {
+    int id;      
+    float cost;  
+};
+int main() {
+    item items[6] =
+    {
+        {1, 50.02},  
+        {2, 25.00},  
+        {3, 50.00}, 
+        {4, 100.59}, 
+        {5, 89.05},  
+        {6, 89.99}   
+    };
+    for (int i = 0; i < 6; i++) {
 
+        if (items[i].cost > 50.00) 
+            cout << "Item ID: " << items[i].id << ", Cost: " << items[i].cost<<endl;
+    }
+return 0;
+}
